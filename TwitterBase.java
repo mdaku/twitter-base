b0e42251c170b64b6f3d73b9776fe3ed23d693d3
Mark <markdaku@Marks-MacBook-Pro.local>
2016-04-19 18:59:26 -0400

    Initializing local repo

---
+++ TwitterBase.java
@@ -0,0 +1,15 @@
+public TwitterBase{
+	// Provides the base template for any Twitter app - other apps must extend this in order to be functional
+
+	public TwitterBase(String credentials){
+		// Points towards a credentials file, where we read all the twitter crap
+	}
+
+
+	// Private helper methods
+	private String readCredentials(String cred){
+
+	}
+
+
+}
